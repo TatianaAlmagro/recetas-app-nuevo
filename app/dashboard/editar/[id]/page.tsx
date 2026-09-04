7fc6f51daffc937f627066fa33c20b8971ca5577
TatianaAlmagro <marialetasianacarpio@gmail.com>
2026-09-04 14:49:42 -0500

    Cambia rutas @/ por rutas relativas

--- app/dashboard/editar/[id]/page.tsx
+++ app/dashboard/editar/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '../../../lib/supabase'
 import { useParams, useRouter } from 'next/navigation'
 
 export default function EditarRecetaPage() {
